dcd73a4e7c90f9b92c2cf9922e6fc2d5ad4f848e
Ltc <1220096650@qq.com>
2020-01-06 19:44:56 +0800

    Revert "test"
    This reverts commit 5e15a3c1

--- src/test/java/org/ruiyun/sse/TestIndexing.java
+++ src/test/java/org/ruiyun/sse/TestIndexing.java
@@ -9,7 +9,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -27,14 +27,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class TestIndexing {
-  /**
-   *
-   *
-   * @param args
-   * @throws Exception
-   */
-	public static void main(String[] args) throws Exception {
 
+	public static void main(String[] args) throws Exception {
+		
 		Printer.addPrinter(new Printer(Printer.LEVEL.EXTRA));
 
 		BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
